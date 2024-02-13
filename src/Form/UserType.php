662e5fe11ada7c0268b8d94bc607b678cfd5dbf1
vincent martin <vinmart@outlook.fr>
2024-02-13 11:57:27 +0100

    amélioration et ajout des champs pour le formulaire registrationform et new user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,22 +6,55 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-        ;
+            ->add(
+                'lastname', TextType::class, [
+                'label' => 'Nom :'
+                ]
+            )
+            ->add(
+                'firstname', TextType::class, [
+                'label' => 'Prénom :'
+                ]
+            )
+            ->add(
+                'email', EmailType::class, [
+                'label' => 'Email :'
+                ]
+            )
+            ->add(
+                'phone', TelType::class, [
+                'label' => 'Téléphone :'
+                ]
+            )
+            //->add('roles')
+            ->add(
+                'password', PasswordType::class, [
+                'label' => 'Mot de passe :'
+                ]
+            )
+            ->add(
+                'confirmpassword', PasswordType::class, [
+                'label' => 'Confirmer le mot de passe :'
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
+        $resolver->setDefaults(
+            [
             'data_class' => User::class,
-        ]);
+            ]
+        );
     }
 }
