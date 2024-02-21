f0b270ced4b024f0d2f66ca9af6910bb3cde1222
IPlkt <ipalakot@guinot.asso.fr>
2024-02-21 14:50:34 +0100

    Revert "création du formulaire d'inscription pour les traders réussi"
    This reverts commit 635c44308ce8e6406c70f9a08f5778a6609cfef0.

--- src/Form/TraderType.php
+++ src/Form/TraderType.php
@@ -2,96 +2,42 @@
 
 namespace App\Form;
 
-use App\Entity\Trader;
 use App\Entity\Activitytype;
+use App\Entity\Trader;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 
 class TraderType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add(
-                'lastnameboss', TextType::class, [
-                'label'=> 'Nom du responsable légal :',
-                ]
-            )
-            ->add(
-                'firstnameboss', TextType::class, [
-                'label' => 'Prénom du responsable légal :', 
-                ]
-            )
-            ->add(
-                'email', EmailType::class, [
-                'label' => 'Email :', 
-                ]
-            )
-            ->add(
-                'password', PasswordType::class, [
-                'label' => 'Mot de passe :', 
-                ]
-            )
-            ->add(
-                'confirmpassword', PasswordType::class, [
-                'label' => 'Confirmer le mot de passe :', 
-                ]
-            )
-            ->add(
-                'phone', TelType::class, [
-                'label' => 'Téléphone :', 
-                ]
-            )
-            ->add(
-                'compagnyname', TextType::class, [
-                'label' => 'Nom entreprise :',
-                ]
-            )
-            ->add(
-                'siren', NumberType::class, [
-                'label' => 'Numéro Siren :', 
-                ]
-            )
-            ->add(
-                'adress', TextType::class, [
-                'label' => 'Adresse :', 
-                ]
-            )
-            ->add(
-                'postalcode', NumberType::class, [
-                'label' => 'Code postal :', 
-                ]
-            )
-            ->add(
-                'city', TextType::class, [
-                'label' => 'ville :', 
-                ]
-            )
-            ->add(
-                'presentation', TextType::class, [
-                'label'=> 'Présentation :',
-                ]
-            )
-            ->add(
-                'activitytype', EntityType::class, [
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('lastnameboss')
+            ->add('firstnameboss')
+            ->add('confirmpassword')
+            ->add('phone')
+            ->add('compagnyname')
+            ->add('siren')
+            ->add('adress')
+            ->add('postalcode')
+            ->add('city')
+            ->add('presentation')
+            ->add('activitytype', EntityType::class, [
                 'class' => Activitytype::class,
-                ]
-            );
+'choice_label' => 'id',
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults(
-            [
+        $resolver->setDefaults([
             'data_class' => Trader::class,
-            ]
-        );
+        ]);
     }
 }
